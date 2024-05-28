3a029396726b6bf117b921dfc1e12cccc3b01444
Moya Zouhon <Moya Zouhon>
2024-05-28 11:51:15 -0400

    this is first commit (worked on monday)

---
+++ robots/moyazouhon/moyazouhon.java
@@ -0,0 +1,18 @@
+package moyazouhon;
+import robocode.Robot;
+
+import robocode.Robot;
+
+public class moyazouhon extends Robot {
+
+public void run(){
+
+    //two sections
+    //first step
+
+    //handle spawn randomness- want to go to a random corner
+
+   
+}
+    
+}
