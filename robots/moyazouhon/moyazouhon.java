84d67b90e980136ce56929a578abcdbb0a502e03
Moya Zouhon <Moya Zouhon>
2024-05-29 19:32:54 -0400

    finished

--- robots/moyazouhon/moyazouhon.java
+++ robots/moyazouhon/moyazouhon.java
@@ -3,10 +3,12 @@ import java.awt.Color;
 import robocode.Robot;
 import robocode.ScannedRobotEvent;
 import robocode.HitWallEvent;
+import java.util.Random;
+import robocode.HitByBulletEvent;
 
 public class MoyaZouhon extends Robot {
  
-    public void run(){
+    public void run();
 
         setBodyColor(Color.pink);
 		setGunColor(Color.pink); 
@@ -19,18 +21,25 @@ public class MoyaZouhon extends Robot {
 		// controls generic movement for entire game
 		while (true) {
             
-            ahead(200);
-            turnLeft(67);
-            back (50);
-            ahead (500);
-            turnLeft (23);
-			turnGunRight(45);
-            ahead(300);
-            back (50);
-            turnLeft(34);
-            ahead (50);
-            turnLeft (45);
-			turnGunRight(45);
+            Random randomNum= new Random();
+            int num = randomNum.nextInt(500) + 75;
+            Random randomAngle= new Random();
+            int angle= randomAngle.nextInt(360)+20; 
+
+            
+
+            ahead(num);
+            turnLeft(angle);
+            back (num);
+            ahead (num);
+            turnLeft (angle);
+			turnGunRight(angle);
+            ahead(num);
+            back (num);
+            turnLeft(angle);
+            ahead (num);
+            turnLeft (angle);
+			turnGunRight(angle);
 		}
             
             
@@ -38,17 +47,26 @@ public class MoyaZouhon extends Robot {
        public void onScannedRobot(ScannedRobotEvent e) {
         double distance= e.getDistance();    
 
-        if (distance <=50){
+        if (distance <=10){
             fire(5);
         }else if (distance <= 100){
-            fire (3);
+            fire (2);
         }else if (distance<= 200){
             fire(1);
         }else{
             fire(.5);
         }
     }
+    public void onHitByBullet(HitByBulletEvent e) {
+		turnLeft(90 - (getHeading() - e.getHeading()));
+		ahead(500);
+	}
 
+    public void onHitWall(HitWallEvent e) {
+		Random randomAngle= new Random();
+            int angle= randomAngle.nextInt(360);
+        turnLeft(angle);
+	}
 
  }      
     
