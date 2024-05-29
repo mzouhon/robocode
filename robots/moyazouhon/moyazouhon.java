1e32d2ce5036a1ba7337b1f896161599d797b31b
Moya Zouhon <Moya Zouhon>
2024-05-29 14:56:04 -0400

    worked on  today during physics

--- robots/moyazouhon/moyazouhon.java
+++ robots/moyazouhon/moyazouhon.java
@@ -1,18 +1,62 @@
 package moyazouhon;
+import java.awt.Color;
 import robocode.Robot;
+import robocode.ScannedRobotEvent;
+import robocode.HitWallEvent;
 
-import robocode.Robot;
+public class MoyaZouhon extends Robot {
+ 
+    public void run(){
 
-public class moyazouhon extends Robot {
+        setBodyColor(Color.pink);
+		setGunColor(Color.pink); 
+		setRadarColor(Color.pink);
+		setBulletColor(Color.pink);
+		setScanColor(Color.pink);
 
-public void run(){
+       
+        
+		// controls generic movement for entire game
+		while (true) {
+            
+            ahead(200);
+            turnLeft(67);
+            back (50);
+            ahead (500);
+            turnLeft (23);
+			turnGunRight(45);
+            ahead(300);
+            back (50);
+            turnLeft(34);
+            ahead (50);
+            turnLeft (45);
+			turnGunRight(45);
+		}
+            
+            
+       }
+       public void onScannedRobot(ScannedRobotEvent e) {
+        double distance= e.getDistance();    
 
-    //two sections
-    //first step
+        if (distance <=50){
+            fire(5);
+        }else if (distance <= 100){
+            fire (3);
+        }else if (distance<= 200){
+            fire(1);
+        }else{
+            fire(.5);
+        }
+    }
 
-    //handle spawn randomness- want to go to a random corner
 
-   
-}
+ }      
     
-}
+   
+   
+        
+	
+
+
+
+
